b9f311acb17d9678a0c1f35dfba74b688d77e130
Justin <justincoutts@icloud.com>
2021-01-28 09:39:21 +0000

    RR_125_ExecuteinEditMode_Attribute

---
+++ Realm_Rush/Assets/CubeEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.y);
+    }
+}
