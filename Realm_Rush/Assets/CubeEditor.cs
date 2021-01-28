59e522f66c06b5f221c52eeafb17aa0c2a9d417f
Justin <justincoutts@icloud.com>
2021-01-28 11:55:55 +0000

    RR_128_Intro_Coroutines

--- Realm_Rush/Assets/CubeEditor.cs
+++ Realm_Rush/Assets/CubeEditor.cs
@@ -16,14 +16,14 @@ public class CubeEditor : MonoBehaviour
     {
         Vector3 snapPos;
 
-
-
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
 
         textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = snapPos.x / gridSize + ", " + snapPos.z / gridSize;
+        string labelText = snapPos.x / gridSize + ", " + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
        
     }
 }
