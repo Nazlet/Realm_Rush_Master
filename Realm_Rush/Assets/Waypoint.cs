bf7bce1167035fca2445159a07a0a158a2c1fb87
Justin <justincoutts@icloud.com>
2021-01-29 10:47:44 +0000

    RR_131_InstanceVariables_Constants

--- Realm_Rush/Assets/Waypoint.cs
+++ Realm_Rush/Assets/Waypoint.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    Vector2Int gridPos;
+    const int gridSize = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    public Vector2 GetGridPos()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
+            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+            );
+    }
+
+    
     // Update is called once per frame
     void Update()
     {
