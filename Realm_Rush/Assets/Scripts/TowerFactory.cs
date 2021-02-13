43ce0f21ae811265f8a9b56c4cb57f1ec1716e02
Justin <justincoutts@icloud.com>
2021-02-13 13:16:23 +0000

    RR_155_Implementing_Ring_Buffer

---
+++ Realm_Rush/Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour
+{
+    [SerializeField] int towerLimit = 5;
+    [SerializeField] Tower towerPrefab;
+
+    int numTowers = 0;
+
+
+    public void AddTower(Waypoint baseWaypoint)
+    {
+        if (numTowers < towerLimit)
+        {
+            InstantiateNewTower(baseWaypoint);
+        }
+        else
+        {
+            MoveExistingTower();
+        }
+
+    }
+
+    private static void MoveExistingTower()
+    {
+        Debug.Log("Max towers reached");
+        // todo actually move!
+    }
+
+    private void InstantiateNewTower(Waypoint baseWaypoint)
+    {
+        Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
+        baseWaypoint.isPlaceable = false;
+        numTowers++;
+    }
+}
